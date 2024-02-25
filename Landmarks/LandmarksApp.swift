f0d3d3ec8cec74a672e4383e2e27e89ca3aa0d53
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-02-25 14:19:54 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nhicolas Aponte on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
